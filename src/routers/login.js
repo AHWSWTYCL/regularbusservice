c19cd25576b460515149968f365907bc5046cd30
AHWSWTYCL <2190844544@qq.com>
2021-11-29 20:50:52 +0800

    注册功能

--- src/routers/login.js
+++ src/routers/login.js
@@ -2,7 +2,7 @@ let express = require('express')
 let router = express.Router()
 let User = require('../models/user/user')
 
-router.post('/login', function (req, res,) {
+router.post('/login', function (req, res) {
     let name = req.body.name;
     let password = req.body.password
 
@@ -11,6 +11,33 @@ router.post('/login', function (req, res,) {
         if (err || doc.length === 0) {
             result.code = -1
             res.json(result)
+        } else {
+            result.code = 1
+            res.json(result)
+        }
+    })
+})
+
+router.post('/register', function (req, res) {
+    let name = req.body.name
+    let password = req.body.password
+
+    let result;
+
+    User.find({name: name, password}, (err, doc) => {
+        let result = {};
+        if (err || doc.length === 0) {
+
+            new User({name: name, password: password})
+                .save(function (err, ret) {
+                    if (err) {
+                        result.code = -1;
+                        res.json(result)
+                    } else {
+                        result.code = 1
+                        res.json(result)
+                    }
+                })
         } else {
             result.code = 1
             res.json(result)
